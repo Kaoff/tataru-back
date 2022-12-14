150b562c585dcd2a3ae955c0d0eb1e646b5e8b12
Bastien WEBER <weber.bastien@gmail.com>
2022-12-14 14:36:21 +0100

    :white_check_mark: Add tests for freecompany controller and service

--- src/free-company/free-company.controller.spec.ts
+++ src/free-company/free-company.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FreeCompanyController } from './free-company.controller';
+import { FreeCompanyService } from './free-company.service';
+import { v4 as uuid } from 'uuid';
+import { CreateFreeCompanyDto } from './dto/create-free-company.dto';
+
+const freeCompanyId = uuid();
+const name = 'Free company name';
+const emblemUrls = ['', '', ''];
+const ownerId = uuid();
 
 describe('FreeCompanyController', () => {
     let controller: FreeCompanyController;
@@ -7,6 +15,49 @@ describe('FreeCompanyController', () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [FreeCompanyController],
+            providers: [
+                {
+                    provide: FreeCompanyService,
+                    useValue: {
+                        findAll: jest.fn().mockResolvedValue([
+                            {
+                                id: 'id1',
+                                emblemUrls,
+                                name: 'Test Company 1',
+                                ownerId,
+                            },
+                            {
+                                id: 'id2',
+                                emblemUrls,
+                                name: 'Test Company 2',
+                                ownerId,
+                            },
+                            {
+                                id: 'id3',
+                                emblemUrls,
+                                name: 'Test Company 3',
+                                ownerId,
+                            },
+                        ]),
+                        findOne: jest.fn().mockImplementation((id: string) =>
+                            Promise.resolve({
+                                id,
+                                emblemUrls,
+                                name,
+                                ownerId,
+                            }),
+                        ),
+                        create: jest
+                            .fn()
+                            .mockImplementation((dto: CreateFreeCompanyDto) =>
+                                Promise.resolve({
+                                    id: freeCompanyId,
+                                    ...dto,
+                                }),
+                            ),
+                    },
+                },
+            ],
         }).compile();
 
         controller = module.get<FreeCompanyController>(FreeCompanyController);
@@ -14,5 +65,62 @@ describe('FreeCompanyController', () => {
 
     it('should be defined', () => {
         expect(controller).toBeDefined();
+    });
+
+    describe('findAll', () => {
+        it('should return an array of freeCompanies', async () => {
+            await expect(controller.findAll({})).resolves.toEqual([
+                {
+                    id: 'id1',
+                    emblemUrls,
+                    name: 'Test Company 1',
+                    ownerId,
+                },
+                {
+                    id: 'id2',
+                    emblemUrls,
+                    name: 'Test Company 2',
+                    ownerId,
+                },
+                {
+                    id: 'id3',
+                    emblemUrls,
+                    name: 'Test Company 3',
+                    ownerId,
+                },
+            ]);
+        });
+    });
+
+    describe('findOne', () => {
+        it('should return a single freeCompany with correct id', async () => {
+            await expect(controller.findOne('uuid')).resolves.toEqual({
+                id: 'uuid',
+                emblemUrls,
+                name,
+                ownerId,
+            });
+            await expect(controller.findOne('other uuid')).resolves.toEqual({
+                id: 'other uuid',
+                emblemUrls,
+                name,
+                ownerId,
+            });
+        });
+    });
+
+    describe('create', () => {
+        it('should return the created freeCompany', async () => {
+            const dto: CreateFreeCompanyDto = {
+                name,
+                emblemUrls,
+                ownerId,
+            };
+
+            await expect(controller.create(dto)).resolves.toEqual({
+                id: freeCompanyId,
+                ...dto,
+            });
+        });
     });
 });
--- src/free-company/free-company.service.spec.ts
+++ src/free-company/free-company.service.spec.ts
@@ -1,12 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FreeCompanyService } from './free-company.service';
+import { v4 as uuid } from 'uuid';
+import { PrismaService } from '../prisma/prisma.service';
+
+const testName = 'Test Company 1';
+const testEmblems = ['', '', ''];
+const ownerId = uuid();
+
+const fcArray = [
+    {
+        emblemUrls: testEmblems,
+        name: testName,
+        ownerId,
+    },
+    {
+        emblemUrls: testEmblems,
+        name: 'Test Company 2',
+        ownerId,
+    },
+    {
+        emblemUrls: testEmblems,
+        name: 'Test Company 3',
+        ownerId,
+    },
+];
+
+const db = {
+    freeCompany: {
+        create: jest.fn().mockResolvedValue(fcArray[0]),
+        findMany: jest.fn().mockResolvedValue(fcArray),
+        findUnique: jest.fn().mockResolvedValue(fcArray[0]),
+    },
+};
 
 describe('FreeCompanyService', () => {
     let service: FreeCompanyService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [FreeCompanyService],
+            providers: [
+                FreeCompanyService,
+                {
+                    provide: PrismaService,
+                    useValue: db,
+                },
+            ],
         }).compile();
 
         service = module.get<FreeCompanyService>(FreeCompanyService);
@@ -14,5 +52,31 @@ describe('FreeCompanyService', () => {
 
     it('should be defined', () => {
         expect(service).toBeDefined();
+    });
+
+    describe('create', () => {
+        it('should successfully create a freeCompany', () => {
+            expect(
+                service.create({
+                    name: testName,
+                    emblemUrls: testEmblems,
+                    ownerId,
+                }),
+            ).resolves.toEqual(fcArray[0]);
+        });
+    });
+
+    describe('findAll', () => {
+        it('should return all freeCompanies', async () => {
+            const fcs = await service.findAll({});
+            expect(fcs).toEqual(fcArray);
+        });
+    });
+
+    describe('findOne', () => {
+        it('should return a freeCompany with correct id', async () => {
+            const fc = await service.findOne(uuid());
+            expect(fc).toEqual(fcArray[0]);
+        });
     });
 });
