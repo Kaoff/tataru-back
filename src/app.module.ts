13e0751d45f0f268ebbb5914305835cdbed3e724
Bastien WEBER <weber.bastien@gmail.com>
2023-05-16 20:21:17 +0200

    :construction: WIP Branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { FreeCompanyModule } from './free-company/free-company.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CharacterController } from './character/character.controller';
+import { CharacterService } from './character/character.service';
 
 @Module({
     imports: [FreeCompanyModule, PrismaModule],
-    controllers: [AppController],
-    providers: [AppService, PrismaService],
+    controllers: [AppController, CharacterController],
+    providers: [AppService, PrismaService, CharacterService],
 })
 export class AppModule {}
